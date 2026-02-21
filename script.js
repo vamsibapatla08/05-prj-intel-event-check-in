c29f6e120621d18e9d12c35c042783fab611b966
Vamsi Sai Krishna Bapatla <vamsisaikrishna.bapatla@my.utsa.edu>
2026-02-21 22:43:21 +0000

    Add attendance tracking functionality with form submission handling

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("checkInForm"); //Get's one specific element from the page by its ID and stores it in a variable called form
+const nameInput = document.getElementById("attendeeName"); //Get's one specific element from the page by its ID and stores it in a variable called nameInput
+const teamSelect = document.getElementById("teamSelect"); //Get's one specific element from the page by its ID and stores it in a variable called teamSelect
+
+//Track attendance
+let count = 0;
+const maxCount = 50;
+
+// Handle form submission
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = nameInput.value.trim();
+  const team = teamSelect.value;
+  const teamName = teamSelect.selectedOptions[0].text; // Get the text of the selected option
+
+  console.log(name, teamName);
+
+  //Increment
+  count++;
+  console.log("Total check-ins: " + count);
+
+  //Update progress bar
+  const percentage = Math.round((count / maxCount) * 100) + "%";
+  console.log(`Progress: ${percentage}`);
+
+  //Update team counter
+  const teamCounter = document.getElementById(team + " Count");
+  teamCounter.textContent = parseInt(teamCounter.textContent) + 1;
+
+});
